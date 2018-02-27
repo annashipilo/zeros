918631b77790315109b1e034514b4ce7cb4851e9
annashipilo <a.a.shipilo@gmail.com>
2018-02-28 00:09:22 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var zeros = 0;
+  for(var i = 1; Math.pow(5, i) <= number; i++){
+    zeros += parseInt(number / Math.pow(5,i));
+  }
+  return zeros;
 }
